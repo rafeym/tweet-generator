1c6a72fd18948a86df32bbb6b55b764d6a110d56
Mohammad Rafey <mohammadrafey0@gmail.com>
2022-10-24 19:00:53 -0400

    DEVELOP: share to tweet button

---
+++ src/components/Button/Button.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Button = ({ href }) => {
+    return (
+        <a href={href} target='_blank'>
+            Tweet
+        </a>
+    )
+}
+
+export default Button
